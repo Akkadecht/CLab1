75024f03956605023ea9697fc5ae07ca85b23fd4
6704062610208 <s6704062610208@email.kmutnb.ac.th>
2024-11-28 14:18:41 +0700

    No.1w1

---
+++ Lab1minx.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int num1=14;
+	int num2=23;
+	if(num1>num2){
+        printf("num1 greater than num2");
+	}
+	if(num2>num1){
+		printf("num2 greater than num1");
+	}
+	return 0;
+}
